2f0f313680c55c011164808d4f81350811bda868
juliusronald <juliusronald12@gmail.com>
2025-12-03 23:03:41 +0700

    Tugas 13 – Memasangkan Template dengan Laravel Blade
    *include fixing clean code
    
    Signed-off-by: juliusronald <juliusronald12@gmail.com>

--- Tugas12/Laravel/inventoryapp_juliusronaldchristanto/resources/views/form.blade.php
+++ Tugas12/Laravel/inventoryapp_juliusronaldchristanto/resources/views/form.blade.php
@@ -1,53 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-<body>
-    <h1>
-        Buat Account Baru!
-    </h1>
-        <h2>
-            Sign Up Form
-        </h2>
-            <form action="{{ route('welcome') }}" method="POST">
-            @csrf
+@extends('layouts.master')
+@section ('title')
+    Register
+@endsection
 
-                <label>First Name:</label><br>
-                <input type="text" name="first_name" required><br><br>
+@section('content')
+    <form action="{{ route('welcome') }}" method="POST">
+    @csrf
 
-                <label>Last Name:</label><br>
-                <input type="text" name="last_name" required><br><br>
+        <label>First Name:</label><br>
+        <input type="text" name="first_name" required><br><br>
 
-                <label>Gender:</label><br>
-                <input type="radio" id="male" name="gender" value="male">
-                <label for="male">Male</label><br>
+        <label>Last Name:</label><br>
+        <input type="text" name="last_name" required><br><br>
 
-                <input type="radio" id="female" name="gender" value="female">
-                <label for="female">Female</label><br>
+        <label>Gender:</label><br>
+        <input type="radio" id="male" name="gender" value="male">
+        <label for="male">Male</label><br>
 
-                <input type="radio" id="other" name="gender" value="other">
-                <label for="other">Other</label><br><br>
+        <input type="radio" id="female" name="gender" value="female">
+        <label for="female">Female</label><br>
 
-                <label>Nationality</label><br><br>
-                    <select name="nationality"><br>
-                        <option>Indonesian</option>
-                        <option>Singapore</option>
-                        <option>Malaysian</option>
-                        <option>Australian</option>
-                    </select><br><br>
+        <input type="radio" id="other" name="gender" value="other">
+        <label for="other">Other</label><br><br>
 
-                <label>Language Spoken:</label><br>
-                <input type="checkbox" name="lang[]" value="Bahasa Indonesia">Bahasa Indonesia<br>
-                <input type="checkbox" name="lang[]" value="English">English<br>
-                <input type="checkbox" name="lang[]" value="Other">Other<br><br>
+        <label>Nationality</label><br><br>
+            <select name="nationality"><br>
+                <option>Indonesian</option>
+                <option>Singapore</option>
+                <option>Malaysian</option>
+                <option>Australian</option>
+                </select><br><br>
 
-                <label>Bio:</label><br>
-                <textarea name="bio" rows="8" cols="40" required></textarea><br><br>
+        <label>Language Spoken:</label><br>
+            <input type="checkbox" name="lang[]" value="Bahasa Indonesia">Bahasa Indonesia<br>
+            <input type="checkbox" name="lang[]" value="English">English<br>
+            <input type="checkbox" name="lang[]" value="Other">Other<br><br>
 
-                <button type="submit">Sign Up</button>
-            </form>
-</body>
-</html>
+        <label>Bio:</label><br>
+            <textarea name="bio" rows="8" cols="40" required></textarea><br><br>
+
+        <button type="submit">Sign Up</button>
+    </form>
+@endsection
--- Tugas12/Laravel/inventoryapp_juliusronaldchristanto/resources/views/welcome.blade.php
+++ Tugas12/Laravel/inventoryapp_juliusronaldchristanto/resources/views/welcome.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-</head>
-<body>
-    <h1>
-        Selamat Datang {{ $first_name }} {{ $last_name }}
-    </h1>
-    <h2>
-        Terima kasih telah bergabung di SanberBook. Social media kita bersama!
-    </h2>
-</body>
-</html>
+@extends('layouts.master')
+@section ('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <h1>Selamat Datang {{ $first_name }} {{ $last_name }}</h1>
+    <h2>Terima kasih telah bergabung di SanberBook. Social media kita bersama!</h2>
+@endsection
